205c6bb2684c54d866dacee87536cc1ce9a7dbf2
Perolytwo <100368974+Perolytwo@users.noreply.github.com>
2022-11-29 12:46:06 +0100

    Add files via upload

---
+++ src/paqprincipal/tareawhile.java
@@ -0,0 +1,11 @@
+package tema4;
+
+public class tareawhile {
+    public static void main(String[] args) {
+        int numero = 0;
+        while (numero < 3){
+            System.out.println(numero);
+            numero++;
+        }
+    }
+}
