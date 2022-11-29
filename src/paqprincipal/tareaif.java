643865ff653a25231c724a3a4ebf95e4b94bf18d
Perolytwo <100368974+Perolytwo@users.noreply.github.com>
2022-11-29 12:46:51 +0100

    Add files via upload

---
+++ src/paqprincipal/tareaif.java
@@ -0,0 +1,14 @@
+package tema4;
+
+public class tareaif {
+    public static void main(String [] args){
+        int numero = 5;
+        if (numero > 0){
+            System.out.println("El numero es Positivo");
+        }else if(numero < 0){
+            System.out.println("El número es negativo");
+        }else{
+            System.out.println("El número es cero");
+        }
+    }
+}
