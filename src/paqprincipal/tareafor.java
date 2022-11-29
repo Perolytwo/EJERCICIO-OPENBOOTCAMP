18beea92f4427cf713222ae9cf203a2906498d38
Perolytwo <100368974+Perolytwo@users.noreply.github.com>
2022-11-29 12:45:27 +0100

    Add files via upload

---
+++ src/paqprincipal/tareafor.java
@@ -0,0 +1,9 @@
+package tema4;
+
+public class tareafor {
+    public static void main(String[] args) {
+        for(int numero =0; numero <=3;numero++){
+            System.out.println(numero);
+        }
+    }
+}
