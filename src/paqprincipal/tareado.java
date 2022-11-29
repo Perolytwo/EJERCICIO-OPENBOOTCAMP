c4ead2b5e5f4fa570177caeae6034dc6d2e54550
Perolytwo <100368974+Perolytwo@users.noreply.github.com>
2022-11-29 12:44:26 +0100

    Add files via upload

---
+++ src/paqprincipal/tareado.java
@@ -0,0 +1,12 @@
+package tema4;
+
+public class tareado {
+    public static void main(String[] args) {
+        int numero = 4;
+        do {
+            System.out.println(numero);
+            numero++;
+        }
+        while(numero <3);
+    }
+}
