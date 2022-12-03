80dea4f0875e9ebe17d89d1c7de0ee448560f626
Perolytwo <100368974+Perolytwo@users.noreply.github.com>
2022-12-03 03:38:18 +0100

    Add files via upload

---
+++ tema_9/herencia.java
@@ -0,0 +1,14 @@
+package tema_9;
+
+public class herencia {
+    public static void main (String[] args){
+
+
+    }
+}
+
+class Vehiculo{
+    int VelolcidadMaxima;
+    String TipoDeGasolina;
+
+}
